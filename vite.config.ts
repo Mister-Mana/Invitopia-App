4daa863c60a10cbab0caef39f33a0b42d86be9a1
Manassé KIKAYA <110218792+Mister-Mana@users.noreply.github.com>
2025-09-19 14:34:45 +0100

    Mise  à jour des codes

--- vite.config.ts
+++ vite.config.ts
@@ -17,6 +17,7 @@ export default defineConfig(({ mode }) => ({
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
+      'fabric': 'fabric/dist/fabric.js',
     },
   },
   // Ensure environment variables are properly loaded
